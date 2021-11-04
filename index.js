dc7b18018ff8bc5d175dcb126b0822d882e37167
Elena Karas <karas2k5@yandex.ru>
2021-11-05 00:38:40 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-gjhg
+console.log("кот">"код"); //true
+console.log(undefined == null); // true
+console.log(undefined === null); //false
+console.log(null==0); //false
+console.log(2 > "3"); //false
+console.log(null - false + true); //NaN
+console.log(1 / "l"); // Nan
+console.log("2" * "3"); // 6
+console.log(4 + 5 + "O"); //90
+console.log("l" + 4 + 5); //l45
+console.log("4" - 2);  //2
+console.log("4" - "4x"); // Nan
+console.log(null == false); // false
+console.log(" -4 "/ 0 + 1); // -Infinity
+console.log(null + 1); // 1
+console.log(undefined + null); // NaN
