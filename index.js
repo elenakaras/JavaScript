0064e15dc396924777d468c829598998be7dcb6c
Elena Karas <karas2k5@yandex.ru>
2021-11-12 01:55:33 +0300

    dz3

--- index.js
+++ index.js
@@ -1,16 +1,172 @@
-console.log("кот">"код"); //true
-console.log(undefined == null); // true
-console.log(undefined === null); //false
-console.log(null==0); //false
-console.log(2 > "3"); //false
-console.log(null - false + true); //NaN
-console.log(1 / "l"); // Nan
-console.log("2" * "3"); // 6
-console.log(4 + 5 + "O"); //90
-console.log("l" + 4 + 5); //l45
-console.log("4" - 2);  //2
-console.log("4" - "4x"); // Nan
-console.log(null == false); // false
-console.log(" -4 "/ 0 + 1); // -Infinity
-console.log(null + 1); // 1
-console.log(undefined + null); // NaN
+// console.log("кот">"код"); //true
+// console.log(undefined == null); // true
+// console.log(undefined === null); //false
+// console.log(null==0); //false
+// console.log(2 > "3"); //false
+// console.log(null - false + true); //NaN
+// console.log(1 / "l"); // Nan
+// console.log("2" * "3"); // 6
+// console.log(4 + 5 + "O"); //90
+// console.log("l" + 4 + 5); //l45
+// console.log("4" - 2);  //2
+// console.log("4" - "4x"); // Nan
+// console.log(null == false); // false
+// console.log(" -4 "/ 0 + 1); // -Infinity
+// console.log(null + 1); // 1
+// console.log(undefined + null); // NaN
+
+
+
+
+
+
+
+
+
+
+// const a=1;
+
+// if(a===0){
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+ 
+
+
+
+// 2
+
+// const a=-3;
+
+// if(a>0){
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+
+
+
+
+
+
+// 4
+
+// const a=-3;
+// if(a>=0){
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+    
+
+
+
+// 3
+
+// const a=-3;
+// if(a<0){
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+
+
+
+
+// 5
+
+// const a=-3;
+// if(a<=0){
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+
+
+
+
+// 6
+
+// const a=-3;
+// if(a!=0){
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+
+
+
+// 7
+
+// let a='3';
+// if(a==='test'){
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+
+
+
+
+// 8
+
+// let a=3;
+// if(a==='1'){
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+
+
+
+
+// логические перем-ые
+
+// 1
+// const a = 'test';
+// if ('test' === false) {
+//     console.log("Верно");
+// } else {
+//     console.log('Неверно');
+// }
+
+
+// 2
+// const a = 'test';
+// if ('test' != false) {
+//     console.log("Верно");
+// } else {
+//     console.log('Неверно');
+// }
+
+
+
+
+// switch-case
+
+const num = '4';
+switch(num){
+    case '1': {
+        console.log('Зима')
+        break;
+    }
+    case '2': {
+        console.log('Весна')
+        break;
+    }
+    case '3': {
+        console.log('Лето')
+        break;
+    }
+    case '4': {
+        console.log('Осень')
+        break;
+    }
+}
+
+
+
+
+
+
